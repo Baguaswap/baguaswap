d10b87d908041a4b0b72036531c70721a31f2793
BaguaSwap <baguaswap@proton.me>
2026-08-12 18:17:20 +0700

    Delete app/Bridge directory

--- app/Bridge/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import BridgeView from "@/components/BridgeView";
-import { useComingSoon } from "@/components/AppShell";
-
-export default function BridgePage() {
-  const onComingSoon = useComingSoon();
-  return <BridgeView onComingSoon={onComingSoon} />;
-}
